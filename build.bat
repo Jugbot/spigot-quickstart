eb5893a6f8814f3943694f32bbf2e69c3ea13fa1
Lucas Pollice <lp1935@nyu.edu>
2021-01-04 20:19:59 -0500

    qs

--- build.bat
+++ build.bat
@@ -1,5 +1,12 @@
-cd plugin
+if not exist server/spigot.jar (
+  cd server
+  call build.bat
+  cd ..
+)
+cd plugin 
 call mvn clean package || goto :end
 cd ..
+if not exist server/plugins mkdir server/plugins
+del .\plugin\target\original*.jar
 copy .\plugin\target\*.jar .\server\plugins
 :end
