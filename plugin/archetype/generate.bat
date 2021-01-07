1bd57bc44ceec5e943440f24f80d68c49c2edad5
Lucas Pollice <lp1935@nyu.edu>
2021-01-06 20:25:25 -0500

    qs

--- plugin/archetype/generate.bat
+++ plugin/archetype/generate.bat
@@ -1,6 +1,9 @@
-@echo off
-mvn install archetype:update-local-catalog
+@echo on
+call mvn clean install archetype:update-local-catalog
 cd ..
-mvn archetype:generate -DarchetypeCatalog=local -DarchetypeArtifactId="sample-archetype" -DarchetypeGroupId="io.github.jugbot" -DoutputDirectory=tmp
-copy tmp/*/ ./
-del tmp
+call mvn archetype:generate -DarchetypeCatalog=local -DarchetypeArtifactId="sample-archetype" -DarchetypeGroupId="io.github.jugbot" -DoutputDirectory=tmp
+
+for /d %%a in (tmp/*) do (
+  xcopy /s tmp\%%a .
+)
+rmdir tmp /s /q
